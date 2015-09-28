c3fc41499af70b5cda7b85eb57de078f2cd89c25
lnwpor <tclnwpor@gmail.com>
2015-09-28 02:44:47 +0000

    edit server to use c9 as a host

--- hungry-joe-server/app.js
+++ hungry-joe-server/app.js
@@ -1,20 +1,16 @@
 var express           = require('express');
+var app               = express();
 var bodyParser 		  = require('body-parser');
 var mongoose          = require('mongoose');
-var cfenv             = require('cfenv');
 
-var app               = express();
-var appEnv            = cfenv.getAppEnv();
-var host = (process.env.VCAP_APP_HOST || 'localhost');
-var port = (process.env.VCAP_APP_PORT || 3000);
+
 mongoose.connect('mongodb://LNWPOR:lnwpor@ds051553.mongolab.com:51553/hungry-joe');
 
 
 
 
-// app.use(bodyParser());
-// app.use(bodyParser.urlencoded());
-// app.use(bodyParser.json());
+app.use(bodyParser());
+
 
 //Cross Origin Request Sharing
 app.use(function(req, res, next) {
@@ -29,21 +25,16 @@ var Users = mongoose.model('Users', {
 });
 
 app.get('/api/users', function(req, res) {
-    // Users.find(function(err, Users) {
-    //     if (err)
-    //         res.send(err)
-    //     res.json(Users);
-    // });
-    
-    // res.writeHead(200, {"Context-Type": "text/plain"});
-    // res.write("Here is some data");
-    // res.end();
+    Users.find(function(err, Users) {
+        if (err)
+            res.send(err)
+        res.json(Users);
+    });
 });
 
-// app.set('port', (process.env.PORT || 5000));
-// app.listen(app.get('port'), function() {
-//   console.log('Node app is running on port', app.get('port'));
-// });
 
-app.listen(port, host);
-console.log('App started on port ' + port);
+app.listen(process.env.PORT,process.env.IP, function() {
+  console.log('I\'m Listening...');
+  console.log(process.env.IP);
+  console.log(process.env.PORT);
+})
