2acc36e18d00aa4168faed719d0f0d70ea37965d
LNWPOR <tclnwpor@gmail.com>
2015-10-03 17:39:11 +0700

    fix server path to hungry-joe-lnwpor-1

--- hungry-joe-client/www/modules/comments/services.js
+++ hungry-joe-client/www/modules/comments/services.js
@@ -1,7 +1,7 @@
 angular.module('Comments',[]).factory('CommentsServices', ['$resource', function ($resource) {
     var CommentsServices = {};
 
-    var base = "https://hungry-joe-lnwpor.c9.io:8080"
+    var base = "https://hungry-joe-lnwpor-1.c9.io:8080"
 	var Comments = $resource(base+'/api/comments');
    
     UsersServices.getUsers = function(){
--- hungry-joe-client/www/modules/restaurantLists/services.js
+++ hungry-joe-client/www/modules/restaurantLists/services.js
@@ -1,7 +1,7 @@
 angular.module('RestaurantLists',[]).factory('RestaurantListsServices', ['$resource', function ($resource) {
     var RestaurantListsServices = {};
 
-    var base = "https://hungry-joe-lnwpor.c9.io:8080"
+    var base = "https://hungry-joe-lnwpor-1.c9.io:8080"
 	var RestaurantLists = $resource(base+'/api/restaurantlists');
    
     UsersServices.getUsers = function(){
