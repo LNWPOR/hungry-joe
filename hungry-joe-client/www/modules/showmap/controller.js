a88b898f803f1baa68b3c42e93f8e61e5d5757e5
Nitit Taepant <Orz@Nitits-MacBook-Pro.local>
2015-10-04 22:16:51 +0700

    search location nearby with radius

--- hungry-joe-client/www/modules/showmap/controller.js
+++ hungry-joe-client/www/modules/showmap/controller.js
@@ -2,14 +2,16 @@ angular.module('Showmap',[])
 .controller('ShowmapController', ['$ionicLoading', function($ionicLoading){
 	var vm = this;
 
+    // set default map
     var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
  
     var mapOptions = {
         center: myLatlng,
         zoom: 16,
         mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+    };    
 
+    // start google map
 	google.maps.event.addDomListenerOnce(window, 'click', function() {
  
         var map = new google.maps.Map(document.getElementById("map"), mapOptions);
@@ -21,13 +23,54 @@ angular.module('Showmap',[])
                 map: map,
                 title: "My Location"
             });
+            
         });
 
+        //button to go current position
         vm.go_Mypos = function(){
             navigator.geolocation.getCurrentPosition(function(pos) {
                 map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
             });
         };
+
+        // set radius search
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            var request = {
+                location: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+                radius: '5000',
+                type: ['food'],
+                name: 'kfc'
+              };
+
+            service.nearbySearch(request, callback);
+        });
+
+        var service = new google.maps.places.PlacesService(map);
+        var infowindow = new google.maps.InfoWindow(); 
+
+        // service.nearbySearch(request, callback);
+        
+
+        function callback(results, status) {
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+              createMarker(results[i]);
+            }
+          }
+        }
+
+        function createMarker(place) {
+          var placeLoc = place.geometry.location;
+          var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location
+          });
+          google.maps.event.addListener(marker, 'click', function() {
+            var infoWindow = new google.maps.InfoWindow(); 
+            infowindow.setContent(place.name);
+            infowindow.open(map, this);
+          });
+        }
 
         vm.map = map;
     });
