8aacf421ce84b5250b0cb144caa07481d51d94e8
LNWPOR <tclnwpor@gmail.com>
2015-10-05 23:47:52 +0700

    add almost all fuction to all database service but still having bug about asynchronous

--- hungry-joe-client/www/modules/restaurantLists/services.js
+++ hungry-joe-client/www/modules/restaurantLists/services.js
@@ -1,11 +1,19 @@
 angular.module('RestaurantLists',[]).factory('RestaurantListsServices', ['$resource', function ($resource) {
     var RestaurantListsServices = {};
-
-    var base = "https://hungry-joe-lnwpor-3.c9.io:8080"
-	var RestaurantLists = $resource(base+'/api/restaurantlists');
+    var base = "https://hungry-joe-lnwpor-3.c9.io:8080";
+    var RestaurantLists = $resource(base+'/api/restaurantlists/:restaurant_id', {restaurant_id : '@restaurant_id'},{
+        update: {
+            method: 'PUT'
+        }
+    });
+	var RestaurantListsGres = $resource(base+'/api/restaurantlists/:gres_id', {gres_id : '@gres_id'});
    
     RestaurantListsServices.getRestaurantLists = function(){
         return RestaurantLists.query();
+    }
+
+    RestaurantListsServices.getRestaurantByGresID = function(gres_id){
+        return RestaurantListsGres.get({gres_id:gres_id});
     }
 
     RestaurantListsServices.addRestaurant = function(name,gres_id){
@@ -13,8 +21,13 @@ angular.module('RestaurantLists',[]).factory('RestaurantListsServices', ['$resou
     	restaurant.name = name;
     	restaurant.gres_id = gres_id;
     	restaurant.rating = 0;
+    	RestaurantLists.save(restaurant);
+    }
 
-    	RestaurantLists.save(restaurant)
+    RestaurantListsServices.ratingUP = function(restaurant_id,rating){
+        var currentRating = rating;
+        var newRating = currentRating + 1;
+        RestaurantLists.update({ _id : restaurant_id }, {rating : newRating});
     }
 
     return RestaurantListsServices;
