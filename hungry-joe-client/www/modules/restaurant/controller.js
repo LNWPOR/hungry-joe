25e5b3a0a3aec2acd4ad46d40ff701995d082ae0
LNWPOR <tclnwpor@gmail.com>
2015-10-11 04:08:13 +0700

    fake real time comment

--- hungry-joe-client/www/modules/restaurant/controller.js
+++ hungry-joe-client/www/modules/restaurant/controller.js
@@ -1,11 +1,6 @@
 angular.module('Restaurant',[])
 .controller('RestaurantController', [ 'RestaurantListsServices' ,'MapvalueServices', 'CommentsServices', 'UsersServices', function(RestaurantListsServices,MapvalueServices,CommentsServices,UsersServices){
 	var vm = this;
-	
-
-
-
-
 
 
 	vm.showComment = false;
@@ -15,17 +10,16 @@ angular.module('Restaurant',[])
 		var commentsPromise = CommentsServices.getRestaurantComments(vm.res._id);
 		commentsPromise.$promise.then(function(commentData){
 			vm.comments = commentData;
-			// vm.comments = [{"description":"test1"},{"description":"test2"}];
 			vm.showComment = true;
 		})
 	});
 	
+	vm.tokens = [];
 	vm.addNewComment = function(){
-		
 		CommentsServices.addComments(vm.description,vm.res._id,UsersServices.getCurrentUsername());
 		console.log('You just comment :' + vm.description);
+		vm.tokens.push({"description":vm.description,"username":UsersServices.getCurrentUsername()});
 		vm.description = '';
-
 	}
 
 }]);
