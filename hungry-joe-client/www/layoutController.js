027e0acca00459f1de9995805ea5a812e5c82fc5
LNWPOR <tclnwpor@gmail.com>
2015-10-12 21:32:43 +0700

    add Logout Button inside ion nav bar

--- hungry-joe-client/www/layoutController.js
+++ hungry-joe-client/www/layoutController.js
@@ -1,4 +1,9 @@
-angular.module('HungryJoe').controller('LayoutController',[function(){
+angular.module('HungryJoe').controller('LayoutController',['UsersServices', '$location', function(UsersServices,$location){
 	var vm = this;
-
+	vm.logout = function () {
+	    UsersServices.logout()
+	      .then(function () {
+	        $location.path('/login');
+	      });
+	};
 }]);
