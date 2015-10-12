1416c4a938585d8aeaca7d92ccdf840f4017824b
Nonthaphat37 <nonthaphat37@gmail.com>
2015-10-12 14:57:36 +0700

    add search

--- hungry-joe-client/www/modules/search/controller.js
+++ hungry-joe-client/www/modules/search/controller.js
@@ -1,5 +1,26 @@
 angular.module('Search',[])
-.controller('SearchController', [function(){
+.controller('SearchController', function($scope, $filter){
 	var vm = this;
-
-}]);
+	vm.result = [];
+	vm.searchKeyword = function(keyword) {
+		vm.result = $filter('filter')(vm.restaurantDB, keyword);
+	}
+	vm.restaurantDB = [
+	{
+		"name":"KFC",
+		"food":["ไก่ทอด","เบอร์เกอร์"]
+	},
+	{	
+		"name":"Mc",
+		"food":["burger","ผัก", "ไก่ทอด"]
+	},
+	{
+		"name":"Pizza",
+		"food":["ทอด","หมู"]
+	}
+	]
+	vm.sendData = function(){
+		console.log(vm.result.length);
+		console.log(vm.result);
+	}
+});
