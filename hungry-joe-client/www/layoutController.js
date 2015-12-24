25256ffcdf1ffb68742f590a598b8d65644911d5
LNWPOR <tclnwpor@gmail.com>
2015-12-24 13:27:17 +0700

    remove logout button on signup page

--- hungry-joe-client/www/layoutController.js
+++ hungry-joe-client/www/layoutController.js
@@ -7,6 +7,19 @@ angular.module('HungryJoe').controller('LayoutController',['UsersServices', '$lo
 	      });
 	};
 
+	vm.showBackButton = true;
 
+	$rootScope.$on('$stateChangeStart', function (event, next, current) {
+	    
+		if($location.path()=='/signup'){
+		    vm.showBackButton = false;
+		    console.log(vm.showBackButton);
+		}
+		else
+	    {
+		    vm.showBackButton = true;
+	    }
+	    
+	});
 	
 }]);
